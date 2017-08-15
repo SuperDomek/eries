5cb7d30ac49f2cf6d2c79544ae53bcafd784ad54
Dominik Bláha <blahad@sic.czu.cz>
2017-08-15 10:32:33 +0200

    Úprava možností doporučení pro recenzenta.

--- lib/pkp/classes/submission/reviewAssignment/ReviewAssignment.inc.php
+++ lib/pkp/classes/submission/reviewAssignment/ReviewAssignment.inc.php
@@ -508,14 +508,15 @@ class ReviewAssignment extends DataObject {
 	 */
 	function getReviewerRecommendationOptions() {
 
+		// EDIT filter the recommendation types
 		static $reviewerRecommendationOptions = array(
 				'' => 'common.chooseOne',
 				SUBMISSION_REVIEWER_RECOMMENDATION_ACCEPT => 'reviewer.article.decision.accept',
 				SUBMISSION_REVIEWER_RECOMMENDATION_PENDING_REVISIONS => 'reviewer.article.decision.pendingRevisions',
-				SUBMISSION_REVIEWER_RECOMMENDATION_RESUBMIT_HERE => 'reviewer.article.decision.resubmitHere',
-				SUBMISSION_REVIEWER_RECOMMENDATION_RESUBMIT_ELSEWHERE => 'reviewer.article.decision.resubmitElsewhere',
-				SUBMISSION_REVIEWER_RECOMMENDATION_DECLINE => 'reviewer.article.decision.decline',
-				SUBMISSION_REVIEWER_RECOMMENDATION_SEE_COMMENTS => 'reviewer.article.decision.seeComments'
+				//SUBMISSION_REVIEWER_RECOMMENDATION_RESUBMIT_HERE => 'reviewer.article.decision.resubmitHere',
+				//SUBMISSION_REVIEWER_RECOMMENDATION_RESUBMIT_ELSEWHERE => 'reviewer.article.decision.resubmitElsewhere',
+				SUBMISSION_REVIEWER_RECOMMENDATION_DECLINE => 'reviewer.article.decision.decline'
+				//SUBMISSION_REVIEWER_RECOMMENDATION_SEE_COMMENTS => 'reviewer.article.decision.seeComments'
 		);
 		return $reviewerRecommendationOptions;
 	}
